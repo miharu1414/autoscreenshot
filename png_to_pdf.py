f3645d3e99b20918599fc2a6bb17fbf9e8a7fb74
igaiga0823 <iga3ster.hiroki@gmail.com>
2022-05-04 23:29:27 +0900

    jijijiiji

--- png_to_pdf.py
+++ png_to_pdf.py
@@ -2,11 +2,18 @@ import pyautogui as pag
 from time import sleep
 import os
 import img2pdf
-
-def png_to_pdf():
-  
-
+from natsort import natsorted
+from PIL import Image
 
 
+def png_to_pdf(a):
+  outputpath= "file.pdf"
+  layout = img2pdf.get_layout_fun((img2pdf.mm_to_pt(257), img2pdf.mm_to_pt(182)))
+  with open(outputpath, "wb") as f:
+    f.write(img2pdf.convert([str(i) for i in natsorted(a) if ".png" in i], layout_fun=layout))
+
+a = {"img1.png", "img2.png", "img3.png"}
+
+png_to_pdf(a)
 
 
